a86ffdaec41bf74015ddbd480a80abea52793f16
Du Tong Thinh <dutongthinh@MacBook-Air-di-Thinh.local>
2026-03-09 17:56:56 +0100

    fix

--- app/informazioni/page.tsx
+++ app/informazioni/page.tsx
@@ -31,10 +31,10 @@ export default function InformazioniPage() {
           </h2>
           <div className="grid md:grid-cols-2 gap-12 text-gray-600 text-left text-lg leading-relaxed">
             <p>
-              Il Tulipano, nato a Carpi nel 2010, è situato a due passi dal centro storico della città in via Aldo Moro Interna 1R, vicino allo stadio di Carpi. Da noi troverete un’atmosfera calda e accogliente, quasi famigliare. Ogni dettaglio del nostro ristorante è stato pensato per rendere ancora più piacevole la vostra esperienza e per farvi godere ogni attimo passato insieme a noi. La specialità del Tulipano è sicuramente la pizza
+              Il Tulipano, nato a Carpi nel 2010, è situato a due passi dal centro storico della città in via Aldo Moro Interna 1R, vicino allo stadio di Carpi. Da noi troverete un’atmosfera calda e accogliente, quasi famigliare. Ogni dettaglio del nostro ristorante è stato pensato per rendere ancora più piacevole la vostra esperienza e per farvi godere ogni attimo passato insieme a noi. La specialità del Tulipano è sicuramente la pizza ma non
             </p>
             <p>
-              ma non mancano di certo le proposte a base di pesce e i piatti tradizionali emiliani. A pranzo inoltre potrete sperimentare il nostro menù del giorno, perfetto per chi ha una pausa pranzo breve ma che non vuole rinunciare al gusto del buon cibo.  Al Tulipano verrete accolti con un sorriso e professionalità e potrete scegliere se accomodarvi nella nostra sala principale o nel nostro dehor, riscaldato in inverno e fresco d’estate.
+              mancano di certo le proposte a base di pesce e i piatti tradizionali emiliani. A pranzo inoltre potrete sperimentare il nostro menù del giorno, perfetto per chi ha una pausa pranzo breve ma che non vuole rinunciare al gusto del buon cibo.  Al Tulipano verrete accolti con un sorriso e professionalità e potrete scegliere se accomodarvi nella nostra sala principale o nel nostro dehor, riscaldato in inverno e fresco d’estate.
             </p>
           </div>
         </div>
